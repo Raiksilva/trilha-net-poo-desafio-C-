5458c35c53565938075a840ce1188906b53c87fb
Raiksilva <raiksilva2016@gmail.com>
2022-10-27 00:54:13 -0300

    Enviando desafio de projeto atualizado

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,21 +4,39 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo;
+        private string IMEI;
+        private int Memoria;
 
-        public Smartphone(string numero)
+        public string nomeApp{ get; private set; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine("Realizando ligação...");
         }
 
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+        
+         public void LigaçaoAtendida()
+        {
+            Console.WriteLine("oie, tudo bem? aqui é...");
+        }
+        
+        public void LigaçaoRecebida()
+        {
+            Console.WriteLine("Alo?");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone Nokia = new Nokia(numero: "3541", modelo: "Modelo1", imei: "111112222", memoria: 32);
+Nokia.InstalarAplicativo("Whatsapp");
+Nokia.Ligar();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone Iphone = new Iphone(numero: "5555", modelo: "Iphone 11 pro max", imei: "755228922", memoria: 256);
+Iphone.InstalarAplicativo("Whatsapp");
+Iphone.ReceberLigacao();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Iphone.LigaçaoRecebida();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia.LigaçaoAtendida();
